d1e7451f9b97a9fbdcc204b814ce92ff121f28b0
William Louis <32376514+nwillouis@users.noreply.github.com>
2017-09-29 10:13:27 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,8 @@
+namaku="Natanael William Louis"
+nim="04316044"
+umurku= 20
+
+print("nama:"+namaku)
+print("nim:"+nim)
+print("umurku sekarang:", str(umurku),"tahun")
+print("umurku 5 tahun kedepan:",str(umurku+5),"tahun",str((umurku+5)*12),"bulan")
