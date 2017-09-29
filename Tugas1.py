9c5e633c99e9c3055fdc812a66802e3accc84f9d
William Louis <32376514+nwillouis@users.noreply.github.com>
2017-09-29 08:28:36 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,13 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> nama="Natanael William Louis Raja Guk Guk"
+>>> nim="04316044"
+>>> umur=20
+>>> umur=umur+5
+>>> print("\nBiodata\n",nama,"\n",nim,"\n",umur)
+
+Biodata
+ Natanael William Louis Raja Guk Guk 
+ 04316044 
+ 25
+>>> 
